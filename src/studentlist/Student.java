e7f4fb53b423144b5b74738980f27e8c5c29c582
SameerNeupane1 <sameerneupane37@gmail.com>
2025-09-27 00:41:54 -0400

    Adding the String datatype.

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -12,7 +12,8 @@ public class Student // model the student raw data
 {
     private String name; // variables private 
    private int sid; // principle "encapsulation"
-private int sem;
+private int sem;    
+private String Address;
     /**
      * @return the name
      */
